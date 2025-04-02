9ab753a6dc3bdea87e2a27bca449246f53a0cbf2
Jorge Olate <jorge.olate9@gmail.com>
2025-04-02 13:19:58 -0300

    configuracion primevue

--- src/primevue-config.js
+++ src/primevue-config.js
@@ -6,22 +6,24 @@ import Select from 'primevue/select';
 import Dropdown from 'primevue/dropdown';
 import MultiSelect from 'primevue/multiselect';
 import AutoComplete from 'primevue/autocomplete';
+import Button from 'primevue/button'; // Agregado para los botones
 
 export function setupPrimeVue(app) {
   app.use(PrimeVue, {
     theme: {
-      preset: Aura, //Aura, Material, Lara, Nara
+      preset: Aura, // Aura, Material, Lara, Nara
       options: {
         prefix: 'p',
         darkModeSelector: false,
-        cssLayer: false
+        cssLayer: true
       }
     }
   });
 
   // Registro global de componentes de PrimeVue
-  app.component('Select', Select);
-  app.component('Dropdown', Dropdown);
-  app.component('MultiSelect', MultiSelect);
-  app.component('AutoComplete', AutoComplete);
+  //app.component('Select', Select);
+  //app.component('Dropdown', Dropdown);
+  //app.component('MultiSelect', MultiSelect);
+  //app.component('AutoComplete', AutoComplete);
+  //app.component('Button', Button);
 }
