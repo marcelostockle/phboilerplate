7707e18c3cc7fd5179ef9b843691de4445e30c6f
Jorge Olate <jorge.olate9@gmail.com>
2025-04-30 13:43:54 -0400

    Refactor router configuration: streamline route definitions and improve layout structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
 import LoginView from '@/views/LoginView.vue'
 import SignupView from '@/views/SignupView.vue'
 import NewPasswordView from '@/views/NewPasswordView.vue'
 import ChartView from '@/views/ChartView.vue'
 import PromptView from '@/views/PromptView.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import DesafiosView from '@/views/DesafiosView.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: DefaultLayout,
+    children: [
+      { path: '', name: 'home', component: HomeView },
+      { path: 'about', name: 'about', component: AboutView },
+      { path: 'chart', name: 'chart', component: ChartView },
+      { path: 'promt', name: 'promt', component: PromptView },
+      { path: 'profile', name: 'profile', component: ProfileView },
+      { path: 'desafios', name: 'desafios', component: DesafiosView },
+    ]
+  },
+  // Rutas sin layout
+  { path: '/login', name: 'login', component: LoginView },
+  { path: '/signup', name: 'signup', component: SignupView },
+  { path: '/newpassword', name: 'newpassword', component: NewPasswordView },
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: SignupView,
-    },
-    {
-      path: '/newpassword',
-      name: 'newpassword',
-      component: NewPasswordView,
-    },
-    {
-      path: '/chart',
-      name: 'chart',
-      component: ChartView,
-    },
-    {
-      path: '/promt',
-      name: 'promt',
-      component: PromptView,
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: ProfileView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
